c9d7ab40ec4ae1f9f4a242877ffcdaa3b39b8cd5
czarecoo@gmail.com <czarecoo@gmail.com>
2019-01-03 19:34:58 +0100

    fixed game scaling

--- js/Main.js
+++ js/Main.js
@@ -1,16 +1,38 @@
-let config = {
-	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	backgroundColor: '#FFFFFF',
-	parent: 'game',
-	physics: {
-		default: 'arcade',
-		arcade: { gravity: 0 }
-	},
-	scene: [
-		BootScene, Game
-	]
+var config;
+var game;
+window.onload = function () {
+	config = {
+		type: Phaser.CANVAS,
+		width: 800,
+		height: 600,
+		backgroundColor: '#FFFFFF',
+		physics: {
+			default: 'arcade',
+			arcade: { gravity: 0 }
+		},
+		scene: [
+			BootScene, Game
+		]
+	};
+
+	game = new Phaser.Game(config);
+
+	resize();
+	window.addEventListener("resize", resize, false);
 };
 
-let game = new Phaser.Game(config);
+
+function resize() {
+	var canvas = document.querySelector("canvas");
+	var windowWidth = window.innerWidth;
+	var windowHeight = window.innerHeight;
+	var windowRatio = windowWidth / windowHeight;
+	var gameRatio = config.width / config.height;
+	if (windowRatio < gameRatio) {
+		canvas.style.width = windowWidth + "px";
+		canvas.style.height = (windowWidth / gameRatio) + "px";
+	} else {
+		canvas.style.width = (windowHeight * gameRatio) + "px";
+		canvas.style.height = windowHeight + "px";
+	}
+}
