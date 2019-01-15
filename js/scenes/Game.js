42648cf1616384740cb727746e416081ef73d009
czarecoo@gmail.com <czarecoo@gmail.com>
2019-01-15 16:01:54 +0100

    Added loading bar

--- js/scenes/Game.js
+++ js/scenes/Game.js
@@ -6,6 +6,64 @@ class Game extends Phaser.Scene {
 		this.chosenClass = data.class;
 	}
 	preload() {
+		var progressBar = this.add.graphics();
+		var progressBox = this.add.graphics();
+		progressBox.fillStyle(0x888888, 0.8);
+		var width = this.game.canvas.width;
+		var height = this.game.canvas.height;
+		progressBox.fillRect(width / 2 - 200, height / 2 - 20, 400, 25);
+
+		var loadingText = this.make.text({
+			x: width / 2,
+			y: height / 2 - 50,
+			text: 'Loading...',
+			style: {
+				font: '20px Arial',
+				fill: '#000000'
+			}
+		});
+		loadingText.setOrigin(0.5, 0.5);
+
+		var percentText = this.make.text({
+			x: width / 2,
+			y: height / 2 - 5,
+			text: '0%',
+			style: {
+				font: '18px Arial',
+				fill: '#000000'
+			}
+		});
+		percentText.setOrigin(0.5, 0.5);
+
+		var assetText = this.make.text({
+			x: width / 2,
+			y: height / 2 + 50,
+			text: '',
+			style: {
+				font: '18px Arial',
+				fill: '#000000'
+			}
+		});
+		assetText.setOrigin(0.5, 0.5);
+		this.load.on('progress', function (value) {
+			percentText.setText(parseInt(value * 100) + '%');
+			progressBar.clear();
+			progressBar.fillStyle(0xffffff, 1);
+			progressBar.fillRect(250, 280, 300 * value, 30);
+		});
+
+		this.load.on('fileprogress', function (file) {
+			assetText.setText('Loading asset: ' + file.key);
+		});
+
+		this.load.on('complete', function () {
+			progressBar.destroy();
+			progressBox.destroy();
+			loadingText.destroy();
+			percentText.destroy();
+			assetText.destroy();
+		});
+
 		Loader.loadAll(this);
 		this.load.scenePlugin({
 			key: 'rexuiplugin',
